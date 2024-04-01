63dcb052136bbb1d85be64a5309358b7482590a3
Darly Parra <darlyg.parras@unac.edu.co>
2024-03-31 23:23:12 -0500

    Punto1

---
+++ ej1.js
@@ -0,0 +1,20 @@
+
+let sumarCuadros =(array)=>{
+    for(let numero in array){
+    
+    
+    cuadroString = "";
+    for (var i = 0; i < 6; i++) {
+        cuadroString += "------\n";
+    }
+    cuadroString += "| " + numero + " |\n";
+    for (var i = 0; i < 6; i++) {
+        cuadroString += "------\n";
+    }
+    
+    // Mostrar el cuadro
+    cuadro.innerText = cuadroString;
+}
+}
+
+console.log(sumarCuadros([[1,23,453,3267,12354, 123456]])) // ["pruebas", "tokens"]
